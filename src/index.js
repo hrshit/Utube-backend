a0a188567acf11ee841c8dad6c52cd3dafa36c7e
Harshit kumar <122025308+hrshit@users.noreply.github.com>
2024-10-14 11:09:46 +0530

    middlewares  wrappers and error handling

--- src/index.js
+++ src/index.js
@@ -9,6 +9,18 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.on("error", (error)=>{
+        console.log("ERR: ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log("Mongo db Connection Failed !!!", error)
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,33 @@
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req,res, next)).
+        catch((err)=> next(err))
+    }
+}
+
+export {asyncHandler};
+
+
+
+
+
+
+
+
+
+
+
+// const asyncHandler = (func) => async () => {}
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try{
+//         await fun(req, res, next)
+//     }
+//     catch(error){
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
